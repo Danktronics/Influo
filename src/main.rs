78e4c06396c0e57b0d95149f1a1c470ed4dec581
Seif <sei4or@gmail.com>
2020-04-27 16:10:29 -0700

    test config code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::fs;
+use std::io;
+
+use serde_json::Value;
+
 fn main() {
     println!("Influo is running!");
+
+    println!(read_configuration())
+}
+
+fn read_configuration() -> Result<Value, io:Error> {
+    let opened_file = fs::read_to_string("./config.json");
+    if opened_file.is_err() {
+        return Err(opened_file);
+    }
+
+    return serde_json::from_str(opened_file);
 }

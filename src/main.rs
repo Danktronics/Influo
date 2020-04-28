50635fb23e166d4e01750476e4c9eacd17c68d10
Seif <sei4or@gmail.com>
2020-04-28 14:27:44 -0700

    thread stuff

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,30 @@ use serde_json::Value;
 fn main() -> Result<(), Error> {
     println!("Influo is running!");
 
+    // Load Configuration
     let config: Value = read_configuration()?;
     println!("URL:\n{}", config["projects"]["url"]);
+
+    let update_interval: Value = config["update_interval"];
+    if update_interval.is_null() || !update_interval.is_number() {
+        setup_updater_thread(30);
+    } else {
+        let interval: Option<u64> = update_interval.as_u64();
+        if interval.is_none() || interval > u32::MAX {
+            throw!("The integer provided exceeded the u32 max");
+        }
+        setup_updater_thread(update_interval.unwrap());   
+    }
+
     Ok(())
+}
+
+/// Interval is in milliseconds
+fn setup_updater_thread(interval: u32) {
+    thread::spawn(|| {
+        thread::sleep(Duration::from_millis(interval));
+        println!("Run here")
+    });
 }
 
 fn read_configuration() -> Result<Value, Error> {

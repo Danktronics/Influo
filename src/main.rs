db244bfce06f3f28119da292b36ef3720e35b870
sithsiri <sith@live.com>
2020-04-28 13:26:16 -0500

    moved some things around, and fixed a problem with Rust not using traditional directory dotlinks

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,26 @@
 use std::fs;
-
+use std::io::ErrorKind;
 use serde_json::{Result, Value};
 
 fn main() -> Result<()> {
     println!("Influo is running!");
 
-    let v: Value = match read_configuration() {
-        Ok(v) => v,
-        Err(e) => return Err(e),
-    };
+    let config: &str = &read_configuration()?;
+    let v: Value = serde_json::from_str(config)?;
     println!("URL:\n{}", v["projects"]["url"]);
     Ok(())
 }
 
-fn read_configuration() -> Result<Value> {
-    let file_string = fs::read_to_string("./config.json")
-        .expect("The config file was not found.");
-    let output: &str = &file_string;    // I think this is type conversion from the original String
-    return serde_json::from_str(output);
+fn read_configuration() -> Result<String> {
+    let output = match fs::read_to_string("config.json") {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match fs::read_to_string("./examples/example-config.json") {
+                Ok(default) => default,
+                Err(e) => panic!("Influo was unable to find both the config and the default config: {:?}", e),
+            },
+            other_error => panic!("Unable to open file: {:?}", other_error),
+        },
+    };
+    Ok(output)
 }

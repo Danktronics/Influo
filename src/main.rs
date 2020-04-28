3fa831b8e403cb034e4e8bddb9ae2d95d348ded5
sithsiri <sith@live.com>
2020-04-28 00:58:00 -0500

    IT COMPILED OMG
    my soul may finally rest

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
 use std::fs;
-use std::io;
 
-use serde_json::Value;
+use serde_json::{Result, Value, Error};
 
-fn main() {
+fn main() -> Result<()> {
     println!("Influo is running!");
 
-    println!(read_configuration())
+    let v: Value = match read_configuration() {
+        Ok(v) => v,
+        Err(e) => return Err(e),
+    };
+    println!("URL:\n{}", v["projects"]["url"]);
+    Ok(())
 }
 
-fn read_configuration() -> Result<Value, io:Error> {
-    let opened_file = fs::read_to_string("./config.json");
-    if opened_file.is_err() {
-        return Err(opened_file);
-    }
-
-    return serde_json::from_str(opened_file);
+fn read_configuration() -> Result<Value> {
+    let output: &str = &fs::read_to_string("./config.json")
+        .expect("The config file was not found.");
+    return serde_json::from_str(output);
 }

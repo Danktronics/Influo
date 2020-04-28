03b31c70bd26b53c28289537150144cb80159246
Seif <sei4or@gmail.com>
2020-04-28 11:54:20 -0700

    some tests

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,12 @@ use serde_json::{Result, Value};
 fn main() -> Result<()> {
     println!("Influo is running!");
 
-    let config: &str = &read_configuration()?;
-    let v: Value = serde_json::from_str(config)?;
-    println!("URL:\n{}", v["projects"]["url"]);
+    let config: str = read_configuration()?;
+    println!("URL:\n{}", config["projects"]["url"]);
     Ok(())
 }
 
-fn read_configuration() -> Result<String> {
-    let output = match fs::read_to_string("config.json") {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match fs::read_to_string("./examples/example-config.json") {
-                Ok(default) => default,
-                Err(e) => panic!("Influo was unable to find both the config and the default config: {:?}", e),
-            },
-            other_error => panic!("Unable to open file: {:?}", other_error),
-        },
-    };
-    Ok(output)
+fn read_configuration() -> Result<Value> {
+    let raw_data: fs::read_to_string("config.json")?;
+    serde_json::from_str(raw_data)?
 }
